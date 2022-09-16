2becb9ce63a60ee30b71a0d054dd6240f303c77c
Ezeali <ezealijoshua@gmail.com>
2022-09-15 19:48:20 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdlib.h>
+#include<time.h>
+#include<stdio.h>
+/**
+ * main-main block
+ * Return: 0
+ */
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
